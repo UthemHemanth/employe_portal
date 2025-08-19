52f9cb001b38f63c52663a4a3bd91893a984b332
UthemHemanth <uthemhemanth1905@gmail.com>
2025-08-19 13:57:49 +0530

    ""db chaged"

--- database.js
+++ database.js
@@ -1,11 +1,11 @@
 const {Pool}= require("pg")
 require("dotenv").config()
 const pool=new Pool({
-    user:process.env.DB_USER,
-    password:process.env.DB_PASSWORD,
-    port:process.env.DB_PORT,
-    host:process.env.DB_HOST,
-    database:process.env.DB_DATABASE
+    user:"postgres",
+    password:"@1234",
+    port:5432,
+    host:"localhost",
+    database:"task"
 })
 
 module.exports=pool
