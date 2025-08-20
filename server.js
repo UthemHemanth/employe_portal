4cb3c1b48c0bc064f053e0c781fc68c039664d17
UthemHemanth <uthemhemanth1905@gmail.com>
2025-08-20 15:57:59 +0530

    "update"

--- server.js
+++ server.js
@@ -22,6 +22,9 @@ app.post("/register",async(req,res)=>{
     const {name,email,password}=req.body
 
     try{
+        if (!email.endsWith("@gmail.com")){
+           return  res.status(401).json({message:"User email should end with @gmail.com"})
+        }
         const exsistinguser=await pool.query( "SELECT * FROM employe WHERE email=$1",[email] )
         
         if (exsistinguser.rows.length>0){
@@ -81,17 +84,65 @@ function verifytoken(req,res,next){
 }
 app.get("/profile",verifytoken,async(req,res)=>{
     try{
-        const result = await pool.query("SELECT * FROM employe WHERE ID=$1",[req.employe_member.id])
+        const result = await pool.query("SELECT * FROM employe WHERE id=$1",[req.employe_member.id])
         res.status(200).json(result.rows[0])
     }catch(err){
         res.status(401).json({error:err.message})
     }
 })
 
+app.delete("/delete",verifytoken,async(req,res)=>{
+    try{
+        const result=await pool.query("SELECT * FROM employe WHERE id=$1",[req.employe_member.id])
 
-// app.get("/", (req, res) => {
-//     res.status(200).send("hi");
+        if (result.rows.length===0){
+            return res.status(400).json({message:"User does not exist"})
+        } else if  (result.rows.length>0){
+            await pool.query("DELETE  FROM employe WHERE id=$1",[req.employe_member.id])
+            res.status(200).json({message:"User successfully deleted"})
+        }
+    }catch(err){
+            res.status(401).json({error:err.message})
+        }
+})
+
+// app.delete("/deleteByEmail", async (req, res) => {
+//   const { email } = req.body;
+
+//   try {
+//     // Check if user exists
+//     const result = await pool.query("SELECT * FROM employe WHERE email=$1", [email]);
+
+//     if (result.rows.length === 0) {
+//       return res.status(400).json({ message: "User not found" });
+//     }
+
+//     // Delete user
+//     await pool.query("DELETE FROM employe WHERE email=$1", [email]);
+
+//     return res.status(200).json({ message: "User deleted successfully" });
+//   } catch (err) {
+//     res.status(500).json({ error: err.message });
+//   }
 // });
+
+
+app.put("/update",verifytoken,async(req,res)=>{
+    const{name,email,password}=req.body
+    try{
+        const result =await pool.query("SELECT * FROM employe WHERE id=$1",[req.employe_member.id])
+        
+         if (result.rows.length===0){
+            return res.status(400).json({message:"USer not found"})
+         }
+         const hashedPassword = await bcrypt.hash(password, 10);
+        const updated= await pool.query("UPDATE employe SET name=$1, email=$2, hashedPassword=$3 WHERE id=$4 RETURNING *",[name,email,password,req.employe_member.id])
+        return res.status(200).json({message:"User details updated successfully"})
+         
+    }catch(err){
+        res.status(400).json({error:err.message})
+    }
+})
 
 
 
